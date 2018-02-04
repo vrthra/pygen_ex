a3be4a375837c569d0198fb157497c6f33f661be
Rahul Gopinath <vrthra@users.noreply.github.com>
2018-02-04 14:37:12 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pygen_ex',
+    version='0.1',
+    description='Some Examples for use with pip install',
+    url='http://github.com/vrthra/pygen_ex',
+    packages=['pygen_ex'],
+    install_requires=[],
+    )
